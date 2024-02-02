2f839428bd12b676950d629b88c0f8df0c752097
Jeremy Trifilo <38505188+JeremyDX@users.noreply.github.com>
2024-02-02 16:35:18 -0500

    Update GameCharacterSystem.h

--- GameCharacterSystem.h
+++ GameCharacterSystem.h
@@ -6,11 +6,10 @@
 */
 class GameCharacterSystem
 {
-public:
-	
-	// Intializer will round to the Alignment Provided I.e. 7 with alignement 16 will be a size of 16. Where 17 will be size of 32. 
-	void InitializeCharacterSystem(const int intializer_size, const int alignment);
 
-	// Clear memory when we're done with this system.
-	void ShutdownCharacterSystem();
+	public:
+	   // Intializer will round to the Alignment Provided I.e. 7 with alignement 16 will be a size of 16. Where 17 will be size of 32. 
+		void InitializeCharacterSystem(const int intializer_size, const int alignment);
+	   // Clear memory when we're done with this system.
+		void ShutdownCharacterSystem();
 };
